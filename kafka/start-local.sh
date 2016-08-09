ccc8a57670830f414d43f79bd81e310c55641f23
Wispy <owispyo@gmail.com>
2016-08-09 05:49:03 -0400

    added sleep before kafka init removed build files from submodules

--- kafka/start-local.sh
+++ kafka/start-local.sh
@@ -6,6 +6,8 @@ export ZLOGS=$KAFKA_HOME/logs/zookeeper.log
 echo "Starting ZooKeeper, logs: $ZLOGS"
 nohup $KAFKA_HOME/bin/zookeeper-server-start.sh $KAFKA_HOME/config/zookeeper.properties > $ZLOGS 2>&1 &
 
+sleep 10
+
 export KLOGS=$KAFKA_HOME/logs/kafka.log
 echo "Starting Kafka, logs: $KLOGS"
 nohup $KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties > $KLOGS 2>&1 &
