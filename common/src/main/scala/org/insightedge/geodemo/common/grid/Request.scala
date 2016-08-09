33a543d0465fb18a07484ea2ab38dd70b31fcdcd
Wispy <owispyo@gmail.com>
2016-08-09 12:04:40 -0400

    added javadoc

--- common/src/main/scala/org/insightedge/geodemo/common/grid/Request.scala
+++ common/src/main/scala/org/insightedge/geodemo/common/grid/Request.scala
@@ -6,6 +6,15 @@ import org.insightedge.scala.annotation.SpaceId
 
 import scala.beans.BeanProperty
 
+/**
+  * Model object stored in the Data Grid
+  *
+  * @param id              the unique id of the request
+  * @param time            the timestamp of request creation
+  * @param latitude        the latitude of request coordinates
+  * @param longitude       the longitude of request coordinates
+  * @param nearRequestsIds the list of detected nearby requests
+  */
 case class Request(
 
                     @BeanProperty
